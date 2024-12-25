2ac7f537516e9710d4ae3fe79204845bfd713a6c
MD SHADAB ALAM <md_shadab_alam@outlook.com>
2024-12-25 23:02:28 +0100

    Update cyclegan_turbo.py

--- img2turbo/src/cyclegan_turbo.py
+++ img2turbo/src/cyclegan_turbo.py
@@ -6,10 +6,10 @@ import torch.nn as nn
 from transformers import AutoTokenizer, CLIPTextModel
 from diffusers import AutoencoderKL, UNet2DConditionModel
 from peft import LoraConfig
-from peft.utils import get_peft_model_state_dict
+from peft.utils import get_peft_model_state_dict  # noqa:F401
 p = "src/"
 sys.path.append(p)
-from model import make_1step_sched, my_vae_encoder_fwd, my_vae_decoder_fwd, download_url
+from model import make_1step_sched, my_vae_encoder_fwd, my_vae_decoder_fwd, download_url  # noqa:E402
 
 
 class VAE_encode(nn.Module):
@@ -53,7 +53,7 @@ def initialize_unet(rank, return_lora_module_names=False):
     l_grep = ["to_k", "to_q", "to_v", "to_out.0", "conv", "conv1", "conv2", "conv_in",
               "conv_shortcut", "conv_out", "proj_out", "proj_in", "ff.net.2", "ff.net.0.proj"]
     for n, p in unet.named_parameters():
-        if "bias" in n or "norm" in n: continue
+        if "bias" in n or "norm" in n: continue  # noqa:E701
         for pattern in l_grep:
             if pattern in n and ("down_blocks" in n or "conv_in" in n):
                 l_target_modules_encoder.append(n.replace(".weight",""))
@@ -64,9 +64,12 @@ def initialize_unet(rank, return_lora_module_names=False):
             elif pattern in n:
                 l_modules_others.append(n.replace(".weight",""))
                 break
-    lora_conf_encoder = LoraConfig(r=rank, init_lora_weights="gaussian",target_modules=l_target_modules_encoder, lora_alpha=rank)
-    lora_conf_decoder = LoraConfig(r=rank, init_lora_weights="gaussian",target_modules=l_target_modules_decoder, lora_alpha=rank)
-    lora_conf_others = LoraConfig(r=rank, init_lora_weights="gaussian",target_modules=l_modules_others, lora_alpha=rank)
+    lora_conf_encoder = LoraConfig(r=rank, init_lora_weights="gaussian",
+                                   target_modules=l_target_modules_encoder, lora_alpha=rank)
+    lora_conf_decoder = LoraConfig(r=rank, init_lora_weights="gaussian",
+                                   target_modules=l_target_modules_decoder, lora_alpha=rank)
+    lora_conf_others = LoraConfig(r=rank, init_lora_weights="gaussian",
+                                  target_modules=l_modules_others, lora_alpha=rank)
     unet.add_adapter(lora_conf_encoder, adapter_name="default_encoder")
     unet.add_adapter(lora_conf_decoder, adapter_name="default_decoder")
     unet.add_adapter(lora_conf_others, adapter_name="default_others")
@@ -85,10 +88,14 @@ def initialize_vae(rank=4, return_lora_module_names=False):
     vae.requires_grad_(True)
     vae.train()
     # add the skip connection convs
-    vae.decoder.skip_conv_1 = torch.nn.Conv2d(512, 512, kernel_size=(1, 1), stride=(1, 1), bias=False).cuda().requires_grad_(True)
-    vae.decoder.skip_conv_2 = torch.nn.Conv2d(256, 512, kernel_size=(1, 1), stride=(1, 1), bias=False).cuda().requires_grad_(True)
-    vae.decoder.skip_conv_3 = torch.nn.Conv2d(128, 512, kernel_size=(1, 1), stride=(1, 1), bias=False).cuda().requires_grad_(True)
-    vae.decoder.skip_conv_4 = torch.nn.Conv2d(128, 256, kernel_size=(1, 1), stride=(1, 1), bias=False).cuda().requires_grad_(True)
+    vae.decoder.skip_conv_1 = torch.nn.Conv2d(512, 512, kernel_size=(1, 1),
+                                              stride=(1, 1), bias=False).cuda().requires_grad_(True)
+    vae.decoder.skip_conv_2 = torch.nn.Conv2d(256, 512, kernel_size=(1, 1),
+                                              stride=(1, 1), bias=False).cuda().requires_grad_(True)
+    vae.decoder.skip_conv_3 = torch.nn.Conv2d(128, 512, kernel_size=(1, 1),
+                                              stride=(1, 1), bias=False).cuda().requires_grad_(True)
+    vae.decoder.skip_conv_4 = torch.nn.Conv2d(128, 256, kernel_size=(1, 1),
+                                              stride=(1, 1), bias=False).cuda().requires_grad_(True)
     torch.nn.init.constant_(vae.decoder.skip_conv_1.weight, 1e-5)
     torch.nn.init.constant_(vae.decoder.skip_conv_2.weight, 1e-5)
     torch.nn.init.constant_(vae.decoder.skip_conv_3.weight, 1e-5)
@@ -147,7 +154,7 @@ class CycleGAN_Turbo(torch.nn.Module):
             self.timesteps = torch.tensor([999], device="cuda").long()
             self.caption = "driving in the day"
             self.direction = "b2a"
-        
+
         elif pretrained_path is not None:
             sd = torch.load(pretrained_path)
             self.load_ckpt_from_state_dict(sd)
@@ -160,9 +167,12 @@ class CycleGAN_Turbo(torch.nn.Module):
         self.unet.cuda()
 
     def load_ckpt_from_state_dict(self, sd):
-        lora_conf_encoder = LoraConfig(r=sd["rank_unet"], init_lora_weights="gaussian", target_modules=sd["l_target_modules_encoder"], lora_alpha=sd["rank_unet"])
-        lora_conf_decoder = LoraConfig(r=sd["rank_unet"], init_lora_weights="gaussian", target_modules=sd["l_target_modules_decoder"], lora_alpha=sd["rank_unet"])
-        lora_conf_others = LoraConfig(r=sd["rank_unet"], init_lora_weights="gaussian", target_modules=sd["l_modules_others"], lora_alpha=sd["rank_unet"])
+        lora_conf_encoder = LoraConfig(r=sd["rank_unet"], init_lora_weights="gaussian",
+                                       target_modules=sd["l_target_modules_encoder"], lora_alpha=sd["rank_unet"])
+        lora_conf_decoder = LoraConfig(r=sd["rank_unet"], init_lora_weights="gaussian",
+                                       target_modules=sd["l_target_modules_decoder"], lora_alpha=sd["rank_unet"])
+        lora_conf_others = LoraConfig(r=sd["rank_unet"], init_lora_weights="gaussian",
+                                      target_modules=sd["l_modules_others"], lora_alpha=sd["rank_unet"])
         self.unet.add_adapter(lora_conf_encoder, adapter_name="default_encoder")
         self.unet.add_adapter(lora_conf_decoder, adapter_name="default_decoder")
         self.unet.add_adapter(lora_conf_others, adapter_name="default_others")
@@ -180,7 +190,8 @@ class CycleGAN_Turbo(torch.nn.Module):
                 p.data.copy_(sd["sd_other"][name_sd])
         self.unet.set_adapter(["default_encoder", "default_decoder", "default_others"])
 
-        vae_lora_config = LoraConfig(r=sd["rank_vae"], init_lora_weights="gaussian", target_modules=sd["vae_lora_target_modules"])
+        vae_lora_config = LoraConfig(r=sd["rank_vae"], init_lora_weights="gaussian",
+                                     target_modules=sd["vae_lora_target_modules"])
         self.vae.add_adapter(vae_lora_config, adapter_name="vae_skip")
         self.vae.decoder.gamma = 1
         self.vae_b2a = copy.deepcopy(self.vae)
@@ -193,7 +204,7 @@ class CycleGAN_Turbo(torch.nn.Module):
         os.makedirs(ckpt_folder, exist_ok=True)
         outf = os.path.join(ckpt_folder, os.path.basename(url))
         download_url(url, outf)
-        sd = torch.load(outf, weights_only = True)
+        sd = torch.load(outf, weights_only=True)
         self.load_ckpt_from_state_dict(sd)
 
     @staticmethod
@@ -202,7 +213,8 @@ class CycleGAN_Turbo(torch.nn.Module):
         assert direction in ["a2b", "b2a"]
         x_enc = vae_enc(x, direction=direction).to(x.dtype)
         model_pred = unet(x_enc, timesteps, encoder_hidden_states=text_emb,).sample
-        x_out = torch.stack([sched.step(model_pred[i], timesteps[i], x_enc[i], return_dict=True).prev_sample for i in range(B)])
+        x_out = torch.stack([sched.step(model_pred[i], timesteps[i],
+                                        x_enc[i], return_dict=True).prev_sample for i in range(B)])
         x_out_decoded = vae_dec(x_out, direction=direction)
         return x_out_decoded
 
@@ -212,13 +224,13 @@ class CycleGAN_Turbo(torch.nn.Module):
         params_gen = list(unet.conv_in.parameters())
         unet.conv_in.requires_grad_(True)
         unet.set_adapters(["default_encoder", "default_decoder", "default_others"])
-        for n,p in unet.named_parameters():
+        for n, p in unet.named_parameters():
             if "lora" in n and "default" in n:
                 assert p.requires_grad
                 params_gen.append(p)
-        
+
         # add all vae_a2b parameters
-        for n,p in vae_a2b.named_parameters():
+        for n, p in vae_a2b.named_parameters():
             if "lora" in n and "vae_skip" in n:
                 assert p.requires_grad
                 params_gen.append(p)
@@ -228,7 +240,7 @@ class CycleGAN_Turbo(torch.nn.Module):
         params_gen = params_gen + list(vae_a2b.decoder.skip_conv_4.parameters())
 
         # add all vae_b2a parameters
-        for n,p in vae_b2a.named_parameters():
+        for n, p in vae_b2a.named_parameters():
             if "lora" in n and "vae_skip" in n:
                 assert p.requires_grad
                 params_gen.append(p)
@@ -249,6 +261,8 @@ class CycleGAN_Turbo(torch.nn.Module):
             caption_enc = caption_emb
         else:
             caption_tokens = self.tokenizer(caption, max_length=self.tokenizer.model_max_length,
-                    padding="max_length", truncation=True, return_tensors="pt").input_ids.to(x_t.device)
+                                            padding="max_length", truncation=True,
+                                            return_tensors="pt").input_ids.to(x_t.device)
             caption_enc = self.text_encoder(caption_tokens)[0].detach().clone()
-        return self.forward_with_networks(x_t, direction, self.vae_enc, self.unet, self.vae_dec, self.sched, self.timesteps, caption_enc)
+        return self.forward_with_networks(x_t, direction, self.vae_enc, self.unet,
+                                          self.vae_dec, self.sched, self.timesteps, caption_enc)
